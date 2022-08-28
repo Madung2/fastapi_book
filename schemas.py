743172380d2432ec2b3c392bc8942b38247610dc
Yeseul Han aka.Tasha <104334219+Madung2@users.noreply.github.com>
2022-08-29 07:10:57 +0900

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -2,6 +2,10 @@ from typing import List, Optional, Generic, TypeVar
 from pydantic import BaseModel, Field
 from pydantic.generics import GenericModel
 
+
+#schema=데이터베이스의 자료구조, 자료간의 관계 정립
+#일종의 drf 시리얼라이저 같은 역할인듯??
+
 T= TypeVar('T')
 
 class BookSchema(BaseModel):
