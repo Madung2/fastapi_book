779a366bda71bdda87ac41f7fbcd29fe52da4e71
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-28 22:29:23 +0900

    feat table done

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "postgresql://postgres:1234@localhost:5432/dev"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import model
 from config import engine
+import router
 
 model.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,6 @@ app = FastAPI()
 @app.get('/')
 async def Home():
     return "Welcome Home"
+
+
+app.include_router(router.router, prefix='/book', tags=['book'])
